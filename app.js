0e8c0fba69fb5106d322736e998741b255f29fa7
ilya <ilyaseverin1998@gmail.com>
2024-10-31 13:54:53 +0300

    refactor docker

--- app.js
+++ app.js
@@ -17,4 +17,12 @@ app.use(cookieParser());
 app.use("/api/user", require("./routes/users"));
 app.use("/api/employees", require("./routes/employees"));
 
+// Раздача статических файлов из папки client/build
+app.use(express.static(path.join(__dirname, "client", "build")));
+
+// Обработка всех остальных запросов, чтобы отправлять клиентский index.html
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
+
 module.exports = app;
